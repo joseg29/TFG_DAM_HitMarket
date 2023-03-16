8891fa4005ec0ef0765980617ec386bd2b80daf7
Samuel Ortega <samujedi@gmail.com>
2023-03-16 14:07:27 +0100

    añado paquete explora y su clase vista y xml

---
+++ app/src/main/java/com/example/tarea1firebase/Explora/VistaExplora.java
@@ -0,0 +1,16 @@
+package com.example.tarea1firebase.Explora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tarea1firebase.R;
+
+public class VistaExplora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_explora);
+    }
+}
