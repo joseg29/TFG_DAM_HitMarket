87259c5d88806cf54a8003bdcf337e4f90ed31f1
alexpozo <delpozo.alex.23@gmail.com>
2023-03-22 20:03:25 +0100

    " haciendo el activity recuperar contraseña, pequeños cambios"

---
+++ app/src/main/java/com/example/tarea1firebase/RecuperarContrasena.java
@@ -0,0 +1,14 @@
+package com.example.tarea1firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecuperarContrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_contrasena);
+    }
+}
