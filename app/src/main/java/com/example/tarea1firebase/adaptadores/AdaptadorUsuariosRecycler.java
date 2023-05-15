a37dc22abec0d22896ed922b20a8aeb61a01de7f
alexpozo <delpozo.alex.23@gmail.com>
2023-05-15 17:37:06 +0200

    "Documentación JavaDoc AdaptadorUsuariosRecycler"

--- app/src/main/java/com/example/tarea1firebase/adaptadores/AdaptadorUsuariosRecycler.java
+++ app/src/main/java/com/example/tarea1firebase/adaptadores/AdaptadorUsuariosRecycler.java
@@ -1,9 +1,11 @@
 /**
- Esta clase representa el adaptador para el RecyclerView que muestra la lista de usuarios
- explorables por el usuario actual.
- El adaptador se encarga de asociar cada elemento de la lista con su respectiva vista en el
- RecyclerView y gestionar los eventos de interacción de los elementos de la lista, como la
- selección de favoritos o la visualización del perfil de un usuario.
+ * Esta clase representa el adaptador para el RecyclerView que muestra la lista de usuarios
+ * explorables por el usuario actual.
+ * El adaptador se encarga de asociar cada elemento de la lista con su respectiva vista en el
+ * RecyclerView y gestionar los eventos de interacción de los elementos de la lista, como la
+ * selección de favoritos o la visualización del perfil de un usuario.
+ *
+ * @author Samuel Ortega Botias
  */
 package com.example.tarea1firebase.adaptadores;
 
@@ -54,6 +56,7 @@ public class AdaptadorUsuariosRecycler extends RecyclerView.Adapter<AdaptadorUsu
 
     /**
      * Constructor para el adaptador de usuarios.
+     *
      * @param listaUsuarios la lista de usuarios que se mostrarán en el RecyclerView
      */
     public AdaptadorUsuariosRecycler(ArrayList<Usuario> listaUsuarios) {
@@ -63,13 +66,19 @@ public class AdaptadorUsuariosRecycler extends RecyclerView.Adapter<AdaptadorUsu
         gestorFirebase = new GestorFirestore();
     }
 
+    /**
+     * Clase estática que representa el ViewHolder de un elemento en el RecyclerView.
+     * Contiene referencias a los elementos de la vista que se mostrarán en el ViewHolder.
+     */
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private TextView lblNombreUsuario, lblMediaEstrellas, lblUbicacion, lblView3;
         private Button btnFav, btnVerPerf;
         private boolean isFavorite;
         private ImageView fotoPerfil;
+
         /**
          * Constructor para la clase ViewHolder.
+         *
          * @param v la vista que se asociará con el ViewHolder
          */
         public ViewHolder(View v) {
@@ -84,12 +93,14 @@ public class AdaptadorUsuariosRecycler extends RecyclerView.Adapter<AdaptadorUsu
 
         }
     }
+
     /**
      * Este método es llamado por el RecyclerView para crear una nueva vista para un elemento de la lista.
-     * @param parent el ViewGroup en el que se añadirá la vista.
+     *
+     * @param parent   el ViewGroup en el que se añadirá la vista.
      * @param viewType el tipo de vista del elemento
      * @return una nueva instancia de la vista del elemento
-     * */
+     */
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view_usuarios_explora, parent, false);
@@ -99,33 +110,81 @@ public class AdaptadorUsuariosRecycler extends RecyclerView.Adapter<AdaptadorUsu
 
     /**
      * Método que establece los objetos en el ViewHolder
-     * @param holder ViewHolder donde se establecerán los objetos
+     *
+     * @param holder   ViewHolder donde se establecerán los objetos
      * @param position posición del elemento en la lista
      */
     @Override
     public void onBindViewHolder(ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        /*
+         * Obtiene una instancia de la base de datos Firestore.
+         */
         db = FirebaseFirestore.getInstance();
+        /*
+         * Obtiene una instancia de la autenticación Firebase.
+         */
         mAuth = FirebaseAuth.getInstance();
+        /*
+         * Obtiene una instancia de la autenticación Firebase.
+         */
         usuarioActualUid = mAuth.getCurrentUser().getUid();
+        /*
+         * Establece la bandera isFavorite en falso.
+         */
         holder.isFavorite = false;
-
+        /*
+         * Obtiene el documento correspondiente al usuario actual de la colección especificada en COLECCION.
+         * Agrega un listener para ser notificado cuando se complete la operación.
+         */
         db.collection(COLECCION).document(usuarioActualUid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            /**
+             * Método que se llama cuando se completa una tarea para obtener un DocumentSnapshot en Firebase Firestore.
+             * Se encarga de procesar el DocumentSnapshot y actualizar el estado y la apariencia del botón de favoritos en el ViewHolder.
+             *
+             * @param task La tarea completada que contiene el DocumentSnapshot.
+             */
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                /*
+                 * Obtiene el DocumentSnapshot de la tarea completada.
+                 */
                 DocumentSnapshot document = task.getResult();
-
-                //Obtenemos el usuario de la base de datos con todos sus campos
+                /*
+                 * Convierte el DocumentSnapshot en un objeto de la clase Usuario.
+                 */
                 Usuario usuario = document.toObject(Usuario.class);
-
+                /*
+                 * Obtiene la lista de favoritos del usuario.
+                 */
                 favoritos = usuario.getListaFavoritos();
+                /*
+                 * Verifica si la posición actual es válida en la lista de usuarios filtrados.
+                 */
                 if (listaUsuariosFiltrados.size() > position) {
+                    /*
+                     * Verifica si el usuario actual se encuentra en la lista de favoritos.
+                     */
                     if (favoritos.contains(listaUsuariosFiltrados.get(position).getId())) {
+                        /*
+                         * El usuario actual es un favorito.
+                         * Establece la bandera isFavorite en verdadero.
+                         */
                         holder.isFavorite = true;
+                        /*
+                         * Actualiza la apariencia del botón de favoritos con el drawable de corazón relleno.
+                         */
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                             holder.btnFav.setForeground(ContextCompat.getDrawable(holder.itemView.getContext(), R.drawable.corazon_favoritos_relleno));
                         }
                     } else {
+                        /*
+                         * El usuario actual no es un favorito.
+                         * Establece la bandera isFavorite en falso.
+                         */
                         holder.isFavorite = false;
+                        /*
+                         * Actualiza la apariencia del botón de favoritos con el drawable de corazón vacío.
+                         */
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                             holder.btnFav.setForeground(ContextCompat.getDrawable(holder.itemView.getContext(), R.drawable.corazon_favoritos_vacio));
                         }
@@ -134,66 +193,163 @@ public class AdaptadorUsuariosRecycler extends RecyclerView.Adapter<AdaptadorUsu
 
             }
         });
-
+        /*
+         * Llamada al método obtenerMediaResenas() del objeto gestorFirebase.
+         * Se pasa como argumento el ID del usuario en la posición actual de la
+         * listaUsuariosFiltrados.
+         * Se crea una nueva instancia de la interfaz GestorFirestore.Callback y se
+         * proporciona una implementación anónima.
+         */
         gestorFirebase.obtenerMediaResenas(listaUsuariosFiltrados.get(position).getId(), new GestorFirestore.Callback() {
+            /**
+             * Se llama cuando se recibe un resultado exitoso en la obtención de la media de estrellas.
+             * Actualiza el texto del lblMediaEstrellas con el valor de mediaEstrellas convertido a una cadena.
+             *
+             * @param mediaEstrellas El valor de la media de estrellas obtenido.
+             */
             @Override
             public void onSuccess(Object mediaEstrellas) {
                 holder.lblMediaEstrellas.setText(mediaEstrellas.toString());
             }
         });
-
+        /* Establece el texto del lblNombreUsuario con el nombre del usuario en la posición
+         * actual de listaUsuariosFiltrados
+         * Convierte el nombre a mayúsculas utilizando el Locale.ROOT para la configuración
+         * regional predeterminada.
+         */
         holder.lblNombreUsuario.setText(listaUsuariosFiltrados.get(position).getNombre().toUpperCase(Locale.ROOT));
-
+        /* Establece el texto del lblUbicacion con la ciudad del usuario en la posición actual de listaUsuariosFiltrados.
+         * Convierte la ciudad a mayúsculas utilizando el Locale.ROOT para la configuración regional predeterminada.
+         */
         holder.lblUbicacion.setText(listaUsuariosFiltrados.get(position).getCiudad().toUpperCase(Locale.ROOT));
+        /* Si lblView3 no es nulo, establece su texto con el género del usuario en la posición actual de listaUsuariosFiltrados.
+         * Convierte el género a mayúsculas utilizando el Locale.ROOT para la configuración regional predeterminada.
+         */
         if (holder.lblView3 != null) {
             holder.lblView3.setText(listaUsuariosFiltrados.get(position).getGenero().toUpperCase(Locale.ROOT));
         }
 
-
+        /* Verifica si la foto de perfil del usuario en la posición actual de listaUsuariosFiltrados
+         * es igual a la URL de la imagen de perfil por defecto.
+         */
         if (listaUsuariosFiltrados.get(position).getFotoPerfil().equals(holder.itemView.getContext().getString(R.string.urlImagenPerfilPorDefecto))) {
+            /*
+            La foto de perfil es igual a la URL de la imagen de perfil por defecto.
+             */
             holder.fotoPerfil.setScaleType(ImageView.ScaleType.FIT_CENTER);
             try {
+                /*
+                 *Carga la imagen de perfil utilizando Glide y la muestra en el ImageView holder.fotoPerfil.
+                 */
                 Glide.with(holder.itemView.getContext()).load(listaUsuariosFiltrados.get(position).getFotoPerfil()).into(holder.fotoPerfil);
             } catch (Exception e) {
+                /*
+                 * Manejo de excepciones en caso de error al cargar la imagen.
+                 * */
             }
         } else {
+            /*
+             * La foto de perfil no es igual a la URL de la imagen de perfil por defecto.
+             */
             try {
+                /*
+                 * Ajusta la escala de la foto de perfil para llenar el ImageView holder.fotoPerfil.
+                 */
                 holder.fotoPerfil.setScaleType(ImageView.ScaleType.FIT_XY);
+                /*
+                 * Carga la imagen de perfil utilizando Glide y la ajusta al centro dentro del
+                 * ImageView holder.fotoPerfil.
+                 */
                 Glide.with(holder.itemView.getContext()).load(listaUsuariosFiltrados.get(position).getFotoPerfil()).fitCenter().into(holder.fotoPerfil);
             } catch (Exception e) {
+                /*
+                 * Manejo de excepciones en caso de error al cargar la imagen.
+                 */
             }
         }
 
-
+        /*
+         * Define un listener para el botón btnVerPerf. Se ejecutará cuando se haga clic en el botón.
+         */
         holder.btnVerPerf.setOnClickListener(v -> {
+            /*
+             * Crea un Intent para abrir la actividad PerfilUsuario.
+             */
             Intent intent = new Intent(v.getContext(), PerfilUsuario.class);
+            /*
+             * Agrega un extra al Intent con el UidUsuario obtenido del usuario en la posición
+             * actual de listaUsuariosFiltrados.
+             */
             intent.putExtra("UidUsuario", listaUsuariosFiltrados.get(position).getId());
+            /*
+             * Inicia la actividad PerfilUsuario utilizando el contexto de la vista actual.
+             */
             v.getContext().startActivity(intent);
         });
-
+        /*
+         * Define un listener para el botón btnFav. Se ejecutará cuando se haga clic en el botón.
+         */
         holder.btnFav.setOnClickListener(v -> {
-            TransitionDrawable transitionDrawableIda = new TransitionDrawable(new Drawable[]{ContextCompat.getDrawable(v.getContext(), R.drawable.corazon_favoritos_vacio), ContextCompat.getDrawable(v.getContext(), R.drawable.corazon_favoritos_relleno)});
-            TransitionDrawable transitionDrawableVuelta = new TransitionDrawable(new Drawable[]{ContextCompat.getDrawable(v.getContext(), R.drawable.corazon_favoritos_relleno), ContextCompat.getDrawable(v.getContext(), R.drawable.corazon_favoritos_vacio)
+            /*
+             * Crea un objeto TransitionDrawable para realizar una transición entre dos drawables.
+             * El primer drawable es el corazón vacío y el segundo drawable es el corazón relleno.
+             */
+            TransitionDrawable transitionDrawableIda = new TransitionDrawable(new Drawable[]{
+                    ContextCompat.getDrawable(v.getContext(), R.drawable.corazon_favoritos_vacio),
+                    ContextCompat.getDrawable(v.getContext(), R.drawable.corazon_favoritos_relleno)
+            });
+            /*
+             * Crea otro objeto TransitionDrawable para realizar la transición en sentido inverso.
+             * El primer drawable es el corazón relleno y el segundo drawable es el corazón vacío.
+             */
+            TransitionDrawable transitionDrawableVuelta = new TransitionDrawable(new Drawable[]{
+                    ContextCompat.getDrawable(v.getContext(), R.drawable.corazon_favoritos_relleno),
+                    ContextCompat.getDrawable(v.getContext(), R.drawable.corazon_favoritos_vacio)
 
             });
-
+            /*
+             * Verifica si el elemento en la posición actual es un favorito.
+             */
             if (holder.isFavorite) {
+                /*
+                 * Configura la transición del drawable en sentido inverso.
+                 */
                 transitionDrawableVuelta.setCrossFadeEnabled(true);
                 transitionDrawableVuelta.startTransition(300);
-
+                /*
+                 * Establece el drawable de transición en el botón favorito.
+                 */
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     holder.btnFav.setForeground(transitionDrawableVuelta);
                 }
+                /*
+                 * Actualiza la lista de favoritos en Firestore eliminando el ID del usuario actual de la lista.
+                 */
                 db.collection(COLECCION).document(usuarioActualUid).update("listaFavoritos", FieldValue.arrayRemove(listaUsuariosFiltrados.get(position).getId())).addOnSuccessListener(documentReference -> {
+                    /*
+                     * Establece la bandera isFavorite en falso para indicar que el usuario ya no es un favorito.
+                     */
                     holder.isFavorite = false;
                 });
             } else {
+                /*
+                 * Configura la transición del drawable en sentido directo.
+                 */
                 transitionDrawableIda.setCrossFadeEnabled(true);
                 transitionDrawableIda.startTransition(300);
+                /*
+                 * Establece el drawable de transición en el botón favorito.
+                 */
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     holder.btnFav.setForeground(transitionDrawableIda);
                 }
+                /*
+                 * Actualiza la lista de favoritos en Firestore agregando el ID del usuario actual a la lista.
+                 */
                 db.collection(COLECCION).document(usuarioActualUid).update("listaFavoritos", FieldValue.arrayUnion(listaUsuariosFiltrados.get(position).getId())).addOnSuccessListener(documentReference -> {
+                    /*
+                     * Establece la bandera isFavorite en verdadero para indicar que el usuario es un favorito.
+                     */
                     holder.isFavorite = true;
                 });
             }
@@ -204,16 +360,19 @@ public class AdaptadorUsuariosRecycler extends RecyclerView.Adapter<AdaptadorUsu
     }
 
     /**
-     * Método que devuelve la cantidad de items que se encuentra en la lista
+     * Devuelve el número de elementos en la lista de usuarios.
+     *
      * @return cantidad de items en la lista
      */
     @Override
     public int getItemCount() {
         return listaUsuariosFiltrados.size();
     }
+
     /**
      * Método que filtra la lista de usuarios
-     * @param query cadena de texto para filtrar la lista de usuarios
+     *
+     * @param query       cadena de texto para filtrar la lista de usuarios
      * @param progressBar barra de progreso que se mostrará durante el filtrado
      */
     public void filter(String query, ProgressBar progressBar) {
