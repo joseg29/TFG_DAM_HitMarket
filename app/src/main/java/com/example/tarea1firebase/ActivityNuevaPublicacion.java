630b55b6bd42d1e99451fbc8616f019bd9d9df29
alex.delpozo <alexdelpozo64@gmail.com>
2023-05-24 11:06:03 +0200

    "javadoc de ActivityNuevaPublicacion y Publicacion"

--- app/src/main/java/com/example/tarea1firebase/ActivityNuevaPublicacion.java
+++ app/src/main/java/com/example/tarea1firebase/ActivityNuevaPublicacion.java
@@ -27,7 +27,9 @@ import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Calendar;
 import java.util.Date;
-
+/**
+ * Actividad para crear una nueva publicación.
+ */
 public class ActivityNuevaPublicacion extends AppCompatActivity {
     private ImageButton btnImagen;
     private EditText etTexto;
@@ -37,22 +39,38 @@ public class ActivityNuevaPublicacion extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private StorageReference mStorageRef;
     private String usuarioActualUid;
-
+    /**
+     * Método que se ejecuta al crear la actividad.
+     *
+     * @param savedInstanceState El estado anterior de la actividad, si está disponible.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nueva_publicacion);
-
+        /*
+         *  Inicializa el gestor de Firestore
+         * */
         gestorFirestore = new GestorFirestore();
+        /*
+         *  Obtiene la instancia actual de FirebaseAuth
+         * */
         mAuth = FirebaseAuth.getInstance();
+        /*
+         * Obtiene la referencia de Firebase Storage con la carpeta "uploads"
+         * */
         mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
+        /*
+         * Obtiene el UID del usuario actualmente autenticado
+         * */
         usuarioActualUid = mAuth.getCurrentUser().getUid();
-
         btnSubir = findViewById(R.id.btnSubirPublicacion);
         btnCancelar = findViewById(R.id.btnCancelarPublicacion);
         btnImagen = findViewById(R.id.btnSeleccionarImagenPublicacion);
         etTexto = findViewById(R.id.etTextoPublicacion);
-
+        /*
+        *  Listener para seleccionar una imagen de la galería
+        * */
         btnImagen.setOnClickListener(v -> {
             Intent intent = new Intent();
             intent.setType("image/*");
@@ -61,27 +79,65 @@ public class ActivityNuevaPublicacion extends AppCompatActivity {
                     Intent.createChooser(intent, "Seleccione una imagen"),
                     200);
         });
-
+        /*
+         *  Listener para subir la publicación
+         * */
         btnSubir.setOnClickListener(v -> {
+            /*
+             * Obtiene la fecha y hora actual en el formato especificado
+             * */
             String fechaYHora = new SimpleDateFormat("yyyy/MM/dd/HH:mm").format(Calendar.getInstance().getTime());
 
             //Si la publicación es con imagen
             if (imageUri != null) {
+                /*
+                 * Crea una referencia al archivo en Firebase Storage con un nombre único
+                 * */
                 StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
                         + "." + getFileExtension(imageUri));
                 fileReference.putFile(imageUri)
                         .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            /**
+                             * Método que se ejecuta cuando la tarea de carga de la imagen es exitosa.
+                             *
+                             * @param taskSnapshot El resultado de la tarea de carga de la imagen.
+                             */
                             @Override
                             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                /*
+                                 * Obtiene la URL de descarga de la imagen subida
+                                 * */
                                 Task<Uri> uriImagenPerfil = taskSnapshot.getStorage().getDownloadUrl();
                                 uriImagenPerfil.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    /**
+                                     * Método que se ejecuta cuando se obtiene exitosamente la URL de descarga de la imagen.
+                                     *
+                                     * @param uri La URL de descarga de la imagen subida.
+                                     */
                                     @Override
                                     public void onSuccess(Uri uri) {
+                                        /*
+                                         * Crea una nueva instancia de Publicacion con los datos proporcionados
+                                         * */
                                         Publicacion publicacion = new Publicacion(usuarioActualUid, etTexto.getText().toString(), fechaYHora, uri.toString());
+                                        /*
+                                         * Agrega la publicación al array "listaPublicaciones" del usuario en Firestore
+                                         * */
                                         gestorFirestore.anadirValorArray(usuarioActualUid, "listaPublicaciones", publicacion, new GestorFirestore.Callback<String>() {
+                                            /**
+                                             * Método de callback invocado cuando se completa con éxito la operación.
+                                             *
+                                             * @param result El resultado de la operación.
+                                             */
                                             @Override
                                             public void onSuccess(String result) {
+                                                /*
+                                                 * Muestra un mensaje de "Publicado"
+                                                 * */
                                                 Toast.makeText(ActivityNuevaPublicacion.this, "Publicado", Toast.LENGTH_SHORT).show();
+                                                /*
+                                                 * Finaliza la actividad
+                                                 * */
                                                 finish();
                                             }
                                         });
@@ -92,9 +148,25 @@ public class ActivityNuevaPublicacion extends AppCompatActivity {
             }
             //Si la publicación es solo texto
             else {
+                /*
+                 * Crea una nueva instancia de la clase Publicacion con los datos proporcionados,
+                 * indicando una URL de imagen vacía.
+                 */
                 Publicacion publicacion = null;
                 publicacion = new Publicacion(usuarioActualUid, etTexto.getText().toString(), fechaYHora, "");
+                /*
+                * Agrega la publicación al array "listaPublicaciones" del usuario en Firestore
+                * utilizando el método anadirValorArray() del gestorFirestore.
+                * Se proporciona un nuevo objeto de tipo GestorFirestore.Callback<String>() como argumento,
+                * que implementa el método onSuccess() del callback.
+                * */
                 gestorFirestore.anadirValorArray(usuarioActualUid, "listaPublicaciones", publicacion, new GestorFirestore.Callback<String>() {
+                    /**
+                     * Método de callback que se ejecuta cuando se completa exitosamente la operación en Firestore.
+                     * Muestra un mensaje de "Publicado" utilizando un Toast y finaliza la actividad actual.
+                     *
+                     * @param result El resultado de la operación en Firestore (en este caso, no se utiliza).
+                     */
                     @Override
                     public void onSuccess(String result) {
                         Toast.makeText(ActivityNuevaPublicacion.this, "Publicado", Toast.LENGTH_SHORT).show();
@@ -103,22 +175,47 @@ public class ActivityNuevaPublicacion extends AppCompatActivity {
                 });
             }
         });
-
+        /*
+        * Establece un OnClickListener para el botón btnCancelar.
+        * */
         btnCancelar.setOnClickListener(v -> {
+            /*
+             * Finaliza la actividad
+             * */
             finish();
         });
     }
-
+    /**
+     * Método que se ejecuta cuando se obtiene un resultado de otra actividad.
+     *
+     * @param requestCode El código de solicitud enviado a la actividad.
+     * @param resultCode  El código de resultado devuelto por la actividad.
+     * @param data        Los datos resultantes de la actividad.
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && requestCode == 200) {
+            /*
+             * Obtiene la URI de la imagen seleccionada.
+             * */
             imageUri = data.getData();
+            /*
+             * Establece la imagen seleccionada en el botón de imagen.
+             * */
             btnImagen.setImageURI(imageUri);
+            /*
+             * Ajusta la escala de la imagen en el botón de imagen para que se ajuste al tamaño del botón.
+             * */
             btnImagen.setScaleType(ImageView.ScaleType.FIT_XY);
         }
     }
-
+    /**
+     * Obtiene la extensión del archivo a partir de su URI.
+     *
+     * @param uri La URI del archivo.
+     * @return La extensión del archivo.
+     */
     private String getFileExtension(Uri uri) {
         ContentResolver cR = getContentResolver();
         MimeTypeMap mime = MimeTypeMap.getSingleton();
--- app/src/main/java/com/example/tarea1firebase/entidades/Publicacion.java
+++ app/src/main/java/com/example/tarea1firebase/entidades/Publicacion.java
@@ -5,15 +5,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
+/**
+ * Clase que representa una publicación.
+ */
 public class Publicacion implements Serializable {
     private String autorUid, texto, fecha, urlImagenPublicacion;
 
     private List<String> nLikes;
-
+    /**
+     * Constructor vacío de la clase Publicacion.
+     */
     public Publicacion() {
     }
-
+    /**
+     * Constructor de la clase Publicacion.
+     *
+     * @param autorUid             El UID del autor de la publicación.
+     * @param texto                El texto de la publicación.
+     * @param fecha                La fecha de la publicación.
+     * @param urlImagenPublicacion La URL de la imagen de la publicación.
+     */
     public Publicacion(String autorUid, String texto, String fecha, String urlImagenPublicacion) {
         this.autorUid = autorUid;
         this.texto = texto;
@@ -21,43 +32,83 @@ public class Publicacion implements Serializable {
         this.urlImagenPublicacion = urlImagenPublicacion;
         this.nLikes = Collections.emptyList();
     }
-
+    /**
+     * Obtiene el UID del autor de la publicación.
+     *
+     * @return El UID del autor de la publicación.
+     */
     public String getAutorUid() {
         return autorUid;
     }
-
+    /**
+     * Establece el UID del autor de la publicación.
+     *
+     * @param autorUid El UID del autor de la publicación.
+     */
     public void setAutorUid(String autorUid) {
         this.autorUid = autorUid;
     }
-
+    /**
+     * Obtiene el texto de la publicación.
+     *
+     * @return El texto de la publicación.
+     */
     public String getTexto() {
         return texto;
     }
-
+    /**
+     * Establece el texto de la publicación.
+     *
+     * @param texto El texto de la publicación.
+     */
     public void setTexto(String texto) {
         this.texto = texto;
     }
-
+    /**
+     * Obtiene la fecha de la publicación.
+     *
+     * @return La fecha de la publicación.
+     */
     public String getFecha() {
         return fecha;
     }
-
+    /**
+     * Establece la fecha de la publicación.
+     *
+     * @param fecha La fecha de la publicación.
+     */
     public void setFecha(String fecha) {
         this.fecha = fecha;
     }
-
+    /**
+     * Obtiene la lista de likes de la publicación.
+     *
+     * @return La lista de likes de la publicación.
+     */
     public List<String> getnLikes() {
         return nLikes;
     }
-
+    /**
+     * Establece la lista de likes de la publicación.
+     *
+     * @param nLikes La lista de likes de la publicación.
+     */
     public void setnLikes(List<String> nLikes) {
         this.nLikes = nLikes;
     }
-
+    /**
+     * Obtiene la URL de la imagen de la publicación.
+     *
+     * @return La URL de la imagen de la publicación.
+     */
     public String getUrlImagenPublicacion() {
         return urlImagenPublicacion;
     }
-
+    /**
+     * Establece la URL de la imagen de la publicación.
+     *
+     * @param urlImagenPublicacion La URL de la imagen de la publicación.
+     */
     public void setUrlImagenPublicacion(String urlImagenPublicacion) {
         this.urlImagenPublicacion = urlImagenPublicacion;
     }
