3e8643d6b02b038fe7f656639d1fa51116d04f8c
alex.delpozo <alexdelpozo64@gmail.com>
2023-05-25 09:38:08 +0200

    "javadoc de RecuperarContrasena"

--- app/src/main/java/com/example/tarea1firebase/RecuperarContrasena.java
+++ app/src/main/java/com/example/tarea1firebase/RecuperarContrasena.java
@@ -36,15 +36,27 @@ public class RecuperarContrasena extends AppCompatActivity {
 
         btnRecuperarContrasena = findViewById(R.id.btnRecuperar);
         etEmail = findViewById(R.id.etEmailRecuperar);
+        /*
+         * Listener para ejecutar el envio de recuperar contraseña
+         * */
         btnRecuperarContrasena.setOnClickListener(v -> {
             progressBar.setVisibility(View.GONE);
+            /*
+             * Llama al método validarCorreo() para verificar la validez del correo electrónico ingresado.
+             * */
             validarCorreo();
         });
     }
-
+    /**
+     * Valida la dirección de correo electrónico ingresada y realiza una acción si es válida.
+     * Si la dirección de correo electrónico es inválida, muestra un mensaje de error en el campo de correo electrónico.
+     */
     private void validarCorreo() {
         String eMail = etEmail.getText().toString().trim();
-
+        /*
+        * Verifica si el campo de correo electrónico está vacío o si no coincide con el patrón
+        * de dirección de correo electrónico
+        * */
         if (eMail.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(eMail).matches()) {
             progressBar.setVisibility(View.GONE);
             etEmail.setError("Correo inválido");
@@ -52,24 +64,37 @@ public class RecuperarContrasena extends AppCompatActivity {
         }
         envia(eMail);
     }
-
+    /**
+     * Envía un correo electrónico de restablecimiento de contraseña a la dirección proporcionada.
+     * Muestra mensajes de éxito o error según el resultado del envío del correo electrónico.
+     *
+     * @param eMail la dirección de correo electrónico a la cual se enviará el correo de restablecimiento de contraseña
+     */
     private void envia(String eMail) {
         mAuth = FirebaseAuth.getInstance();
+        /*
+         * Envía un correo electrónico de restablecimiento de contraseña a la dirección especificada
+         * */
         mAuth.sendPasswordResetEmail(eMail)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
                         progressBar.setVisibility(View.VISIBLE);
                         Toast.makeText(RecuperarContrasena.this, "Correo Enviado", Toast.LENGTH_SHORT).show();
-                        //Falta hacer un xml para que nos avise de quetodo se ha realizado de forma correct
-                        View view = getLayoutInflater().inflate(R.layout.correo_enviado, null);
 
+                        View view = getLayoutInflater().inflate(R.layout.correo_enviado, null);
+                        /*
+                         * Muestra un AlertDialog personalizado para indicar que se ha enviado el correo
+                         * */
                         AlertDialog.Builder builder = new AlertDialog.Builder(RecuperarContrasena.this);
                         builder.setView(view);
                         builder.setCancelable(false);
                         AlertDialog alertDialog = builder.create();
                         alertDialog.show();
 
-                        // Cerrar el AlertDialog después de un tiempo
+                        /*
+                         * Cierra el AlertDialog después de un cierto tiempo y redirige al usuario a
+                         *  la pantalla de inicio de sesión
+                         * */
                         new Handler().postDelayed(() -> {
                             alertDialog.dismiss();
                             Intent intent = new Intent(RecuperarContrasena.this, Login.class);
@@ -82,6 +107,11 @@ public class RecuperarContrasena extends AppCompatActivity {
                     }
                 });
     }
+    /**
+     * Maneja el evento de presionar el botón de retroceso (Back) en el dispositivo.
+     * Sobrescribe el método predeterminado para redirigir al usuario a la pantalla de inicio de sesión.
+     * Al presionar el botón de retroceso, se inicia una nueva actividad de inicio de sesión y se finaliza la actividad actual.
+     */
     @Override
     public void onBackPressed() {
         super.onBackPressed();
